60f465f51f6c6b02337633175e6065666b3d5569
ortiz488 <103153174+ortiz488@users.noreply.github.com>
2022-04-17 17:16:50 -0700

    Create Shape.cpp

---
+++ Shape.cpp
@@ -0,0 +1,50 @@
+#include "shape.h"
+
+Shape::Shape()
+{
+    shapeID = 0;
+    shape = Line;
+}
+//----------------------------------------------------------------------
+Shape::Shape(int shapeID, QPen pen, QBrush brush, shapeType shape)
+{
+    this->shapeID = shapeID;
+    this->shape = shape;
+}
+//----------------------------------------------------------------------
+Shape::~Shape(){}
+//----------------------------------------------------------------------
+void Shape::setID(int ID)
+{
+    shapeID = ID;
+}
+//----------------------------------------------------------------------
+void Shape::setShape(shapeType s)
+{
+    shape = s;
+}
+//----------------------------------------------------------------------
+void Shape::setPen(qreal width, Qt::PenStyle style, Qt::PenCapStyle cap, Qt::PenJoinStyle join)
+{
+    pen.setWidthF(width);
+    pen.setStyle(style);
+    pen.setCapStyle(cap);
+    pen.setJoinStyle(join);
+    // Pen color? pen default constructor has Qbrush parameter, Qcolor?
+}
+//----------------------------------------------------------------------
+void Shape::setBrush(QColor color, Qt::BrushStyle style)
+{
+    brush.setColor(color);
+    brush.setStyle(style);
+}
+//----------------------------------------------------------------------
+int Shape::getID() const
+{
+    return shapeID;
+}
+//----------------------------------------------------------------------
+// Overloaded operators
+bool operator==(const Shape& ID);
+bool operator<(const Shape& ID);
+//----------------------------------------------------------------------
