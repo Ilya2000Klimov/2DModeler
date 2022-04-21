4e2c95c0ab589202b2c45afdaf0b6715be720644
ortiz488 <103153174+ortiz488@users.noreply.github.com>
2022-04-21 05:06:35 -0700

    Text.h WIP

---
+++ Text.h
@@ -0,0 +1,25 @@
+#ifndef TEXT_H
+#define TEXT_H
+#include <QFont>
+#include "shape.h"
+
+class Text : public Shape
+{
+private:
+    QFont font;
+    QString text;
+    
+public:
+    Text();
+    Text(QFont font);
+    virtual ~Text();
+    void setFont();
+    QFont getFont() const;
+    void setText();
+    QString getText() const;
+    virtual void draw(QPaintDevice *device) override;
+    virtual void move() override;
+    virtual void perimeter() override;
+    virtual void area() override;
+};
+#endif // TEXT_H
