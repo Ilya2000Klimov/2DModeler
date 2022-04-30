802d111f192e2aa645e3518a419da90640f2e19e
Ilya Klimov <53608243+Ilya2000Klimov@users.noreply.github.com>
2022-04-30 13:01:40 -0700

    shape 4/30/2022

--- shapes/shape.cpp
+++ shapes/shape.cpp
@@ -2,5 +2,76 @@
 
 Shape::Shape()
 {
-
+    shapeID = 0;
+    pen = Qt::SolidLine;
+    brush = Qt::SolidPattern;
+    shape = None;
 }
+//----------------------------------------------------------------------
+Shape::Shape(int shapeID, QPen pen, QBrush brush, shapeType shape)
+{
+    this->shapeID = shapeID;
+    this->pen = pen;
+    this->brush = brush;
+    this->shape = None;
+}
+//----------------------------------------------------------------------
+Shape::~Shape(){}
+//----------------------------------------------------------------------
+void Shape::setID(int ID)
+{
+    shapeID = ID;
+}
+//----------------------------------------------------------------------
+void Shape::setShape(shapeType s)
+{
+    shape = s;
+}
+//----------------------------------------------------------------------
+void Shape::setPen(QColor color, int width, Qt::PenStyle style, Qt::PenCapStyle cap, Qt::PenJoinStyle join)
+{
+    pen.setColor(color);
+    pen.setWidthF(width);
+    pen.setStyle(style);
+    pen.setCapStyle(cap);
+    pen.setJoinStyle(join);
+}
+//----------------------------------------------------------------------
+void Shape::setBrush(QColor color, Qt::BrushStyle style)
+{
+    brush.setColor(color);
+    brush.setStyle(style);
+}
+//----------------------------------------------------------------------
+int Shape::getID() const
+{
+    return shapeID;
+}
+//----------------------------------------------------------------------
+shapeType Shape::getShapeType() const
+{
+    return shape;
+}
+//----------------------------------------------------------------------
+QPen& Shape::getPen()
+{
+    return pen;
+}
+//----------------------------------------------------------------------
+QBrush& Shape::getBrush()
+{
+    return brush;
+}
+//----------------------------------------------------------------------
+// Overloaded operators
+bool Shape::operator==(const Shape& ID)
+{
+    return shapeID == ID.shapeID;
+}
+//----------------------------------------------------------------------
+bool Shape::operator<(const Shape& ID)
+{
+    return shapeID < ID.shapeID;
+}
+//----------------------------------------------------------------------
+
