7f7b05733c88fcb6bfc85abb5d8992e7402a6c30
ortiz488 <103153174+ortiz488@users.noreply.github.com>
2022-05-06 00:52:29 -0700

    Update Rectangle.h

--- shapes/Rectangle.h
+++ shapes/Rectangle.h
@@ -1,21 +1,26 @@
-#ifndef LINE_H
-#define LINE_H
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
 #include "shape.h"
 
 class Rectangle : public Shape
 {
+private:
+    int x, y, width, height;
 public:
     Rectangle();
-    Rectangle(int length, int width);
+    Rectangle(int x, int y, int width, int height);
     virtual ~Rectangle();
-    virtual void draw(QPaintDevice *device) override;
-    virtual void move(int x, int y, int points) override;
-    virtual void perimeter() override;
-    virtual void area() override;
-    void setPoints(int w, int l);
-    int getPoints() const;
-private:
-    int length;
-    int width;
+    void setY(int y);
+    void setX(int x);
+    void setHeight(int h);
+    void setWidth(int w);
+    int getHeight() const;
+    int getWidth() const;
+    int getX() const;
+    int getY() const;
+    virtual void draw(QPainter *paint) override;
+    virtual void move(int x, int y) override;
+    virtual double perimeter() override;
+    virtual double area() override;
 };
 #endif // RECTANGLE_H
