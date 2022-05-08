210664df523f17301ad5581881516d3c1ab1b876
palschathuri5 <palschathuri@gmail.com>
2022-05-07 22:12:41 -0700

    Updated polygon

--- shapes/polygon.h
+++ shapes/polygon.h
@@ -2,20 +2,22 @@
 #define POLYGON_H
 #include "shape.h"
 #include <QPointF>
+#include <iostream>
 #include <math.h>
-const int DEFAULT_NUM_PTS = 5;
+const int DEFAULT_NUM_PTS = 8;
 class Polygon:public Shape
 {
 public:
-    Polygon(QPaintDevice* device, int ID =-1);
+    Polygon(int numOfPoints,double x, double y);
     ~Polygon() override;
     virtual void draw(QPainter *paint) override;
-    virtual void move(int x, int y, int points) const; //might need to change int point
+    virtual void move(int x, int y, int points); //might need to change int point
     virtual double perimeter() override;
     virtual double area() override;
 
-    int getPoints(); //function to get the remaining vertices of the polygon
-    int setPoints(int remPoints);
+    int getX(); //function to get the X coord
+    int getY();
+    void setPoints(int Xcoord[],int Ycoord[]);
 private:
     int numOfPoints; // will need to find total polygon points
     const QPointF points[DEFAULT_NUM_PTS];
@@ -23,6 +25,7 @@ private:
     double y;
     double Xcoord[DEFAULT_NUM_PTS];
     double Ycoord[DEFAULT_NUM_PTS];
+
 
 };
 
