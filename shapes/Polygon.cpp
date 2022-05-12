91878760c599b5b91c93011de13235a1e12e969b
ortiz488 <103153174+ortiz488@users.noreply.github.com>
2022-05-12 05:08:58 -0700

    Update Polygon & canvas

--- shapes/Polygon.cpp
+++ shapes/Polygon.cpp
@@ -1,156 +1,64 @@
 #include "Polygon.h"
-#include "../file-parsing/ShapeListingSpecification.h"
+using namespace cs1c
 
-using namespace cs1c;
-
-Polygon::Polygon(QPainter* pPainter) : Shape(pPainter)
+Polygon::Polygon()
 {
-    this->numOfPoints= 0;
-    this->x=0;
-    this->y=0;
+    count = 0;
+    p_point = nullptr;
 }
-
-Polygon::Polygon(int numOfPoints,int x, int y)
+//----------------------------------------------------------------------
+Polygon::Polygon(QPoint* p_point, int count)
 {
-    this->numOfPoints=numOfPoints;
-    this->x=x;
-    this->y=y;
+    this->p_point = p_point;
+    this->count = count;
 }
-
-Polygon::~Polygon(){}
-
-void Polygon::setPoints()
+//----------------------------------------------------------------------
+Polygon::~Polygon() {}
+//----------------------------------------------------------------------
+void Polygon::setPoints(int x, int y)
 {
-   /* for(int i=0;i<numOfPoints;i++)
-    {
-        std::cout<<"\nEnter x-coordinate value: ";
-        std::cin>>Xcoord[i];
-        std::cout<<"\nEnter y-coordinate value: ";
-        std::cin>>Ycoord[i];
-    }*/
-    for(QPointF& point : points)
-    {
-       vPoint.push_back(point);
-    }
+    myPoints.push_back(QPoint(x, y));
+    count++;
 }
-
-void Polygon::setX(int x)
+//----------------------------------------------------------------------
+QVector<QPoint>& Polygon::getPoints()
 {
-    this->x=x;
+    return myPoints;
 }
-
-void Polygon::setY(int y)
+//----------------------------------------------------------------------
+void Polygon::draw(QPaintDevice *device)
 {
-    this->y=y;
-}
-
-int Polygon::getX() const
-{
-   /* for(int index=0;index<numOfPoints;index++)
-    {
-        return Xcoord[index];
-    }
-    return 0;*/
-    return x;
-}
-
-int Polygon::getY() const
-{
-   /* for(int index=0;index<numOfPoints;index++)
-    {
-        return Ycoord[index];
-    }
-    return 0;*/
-    return y;
-}
-
-void Polygon::setPoints(int numOfPoints)
-{
-    this->numOfPoints=numOfPoints;
-}
-
-int Polygon::getPoints() const
-{
-    return numOfPoints;
-}
-
-void Polygon::draw(QPaintDevice* pDevice)
-{
-    paint->begin(pDevice);
+    paint = this;
+    paint->begin(device);
     paint->setPen(getPen());
     paint->setBrush(getBrush());
-    paint->drawPolygon(points,numOfPoints);
+    paint->drawPolygon(myPoints.begin(), count);
     paint->end();
 }
-
+//----------------------------------------------------------------------
 void Polygon::move(int x, int y)
 {
-   /* double moveX=0;
-    double moveY=0;
-    //difference between the first coordinate and the coordinate we are moving to
-    moveX = x-Xcoord[0];
-    moveY=y-Ycoord[0];
 
-    //setting the new coordinates as that particular point, ie the point has moved
-    //int points is the point that is being moved
-    Xcoord[points]+=moveX;
-    Ycoord[points]+=moveY;*/
-
-    double moveX=0;
-    double moveY=0;
-    //difference between the first coordinate and the coordinate we are moving to
-    moveX = x-Xcoord[0];
-    moveY=y-Ycoord[0];
-
-    //setting the new coordinates as that particular point, ie the point has moved
-    //int points is the point that is being moved
-    for(QPointF& point : points)
-    {
-        point.rx() +=moveX;
-        point.ry() +=moveY;
-    }
 }
-
-double Polygon::area()
-{
-    double area=0;
-
-
-    int xnum;
-    int ynum=numOfPoints-1;
-    //shoelace method
-    for(xnum=0;xnum<numOfPoints;xnum++)
-    {
-        //area=Xcoord[ynum]+Ycoord[xnum] *(Ycoord[ynum]-Xcoord[xnum]); //formula for area of a polygon
-        area=vPoint[ynum].x()+vPoint[xnum].y() *(vPoint[ynum].y()-vPoint[xnum].x());
-        ynum=xnum;
-    }
-    area = abs(area/2);
-    return area;
-}
+//----------------------------------------------------------------------
 double Polygon::perimeter()
 {
-    double perimeter=0;
-
-    for(int i=0;i<numOfPoints;i++)
-    {
-        //perimeter+=std::sqrt(pow(Xcoord[i+1]-Xcoord[i],2)+pow(Ycoord[i+1]-Ycoord[i],2));
-        perimeter+=std::sqrt(pow(vPoint[i+1].x()-vPoint[i].x(),2)+pow(vPoint[i+1].y()-vPoint[i].y(),2));
-    }
-    //Calculation for the side of the 0th point and the last point
-    //perimeter+=std::sqrt(pow(Xcoord[numOfPoints-1]-Xcoord[0],2)+pow(Ycoord[numOfPoints-1]-Ycoord[0],2));
-     perimeter+=std::sqrt(pow(vPoint[numOfPoints-1].x()-vPoint[0].x(),2)+pow(vPoint[numOfPoints-1].y()-vPoint[0].y(),2));
-return perimeter;
+    return 0;
 }
-
-void cs1c::Polygon::operator>>(QTextStream& fileStream)
+//----------------------------------------------------------------------
+double Polygon::area()
+{
+    return 0;
+}
+//----------------------------------------------------------------------
+void Polygon::operator>>(QTextStream& fileStream)
 {
     fileStream << "\nShapeId: " << this->getID()
         << "\nShapeType: Ellipse"
         << "\nShapeDimensions: ";
     // Repeat for all but last point
-    int i = 1;
-    while(i < numOfPoints)
+    int i = 0;
+    while(i < count)
     {
          fileStream << points[i - 1].x() << ", " << points[i - 1].y() << ", ";
          i++;
@@ -164,14 +72,14 @@ void cs1c::Polygon::operator>>(QTextStream& fileStream)
         << "\nBrushColor: " << slp::colorResolver.key(this->getBrush().color())
         << "\nBrushStyle: " << slp::brushStyleResolver.key(this->getBrush().style());
 }
-
-void cs1c::Polygon::setDimensions(int dimensions[], int dimensionCount)
+//----------------------------------------------------------------------
+void Polygon::setDimensions(int dimensions[], int dimensionCount)
 {
-    points = new QPoint[dimensionCount];
-    numOfPoints = dimensionCount;
-    for(int i = 0; i < dimensionCount; i += 2)
-    {
-        points[i].setX(dimensions[i]);
-        points[i].setY(dimensions[i + 1]);
-    }
+    count = dimensionCount/2;
+
+    for (int i = 0; i < dimensionCount; i += 2)
+        {
+            setPoints(dimensions[i], dimensions[i+1]);
+        }
 }
+//----------------------------------------------------------------------
--- shapes/Polygon.h
+++ shapes/Polygon.h
@@ -1,45 +1,30 @@
 #ifndef POLYGON_H
 #define POLYGON_H
+#include <QPoint>
+#include <QVector>
 #include "Shape.h"
-#include <QPointF>
-#include <iostream>
-#include <math.h>
 
 namespace cs1c
 {
-class Polygon: public Shape
+class Polygon : public Shape
 {
+private:
+    QVector<QPoint> myPoints;
+    int count;
+    QPoint* p_point;
+
 public:
-    Polygon(QPainter* pPainter);
-    Polygon(int numOfPoints,int x, int y);
-    ~Polygon() override;
-    virtual void draw(QPaintDevice* pDevice) override;
-    virtual void move(int x, int y) override;
-    void move(int x, int y, int points);
-    virtual double perimeter() override;
-    virtual double area() override;
+    Polygon();
+    Polygon(QPoint* p_point, int count);
+    virtual ~Polygon();
+    void setPoints(int, int);
+    QVector<QPoint>& getPoints();
     virtual void operator>>(QTextStream& fileStream) override;
     virtual void setDimensions(int dimensions[], int dimensionCount) override;
-
-    int getX() const; //function to get the X coord
-    int getY() const;
-    void setPoints();
-    int getPoints() const;
-    void setPoints(int );
-    void setX(int x);
-    void setY(int y);
-private:
-    // Note: Don't place variables in global scope
-    static const int DEFAULT_NUM_PTS = 8;
-    int numOfPoints; // variable to find total number of points in the polygon
-    std::vector<QPointF> vPoint; //vector to store the points of the polygon in
-    QPoint* points;// Used b/c QPainter drawPoly is QPoint*
-    double x;
-    double y;
-    double Xcoord[DEFAULT_NUM_PTS];
-    double Ycoord[DEFAULT_NUM_PTS];
-
+    virtual void draw(QPaintDevice *device) override;
+    virtual void move(int x, int y) override;
+    virtual double perimeter() override;
+    virtual double area() override;
 };
 }
-
 #endif // POLYGON_H
