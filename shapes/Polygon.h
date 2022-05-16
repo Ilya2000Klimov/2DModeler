b1fb017194189af61e16d3a82ccd3cad1bac5bc3
palschathuri5 <palschathuri@gmail.com>
2022-05-16 15:45:31 -0700

    Doxygen comments for polygon added

--- shapes/Polygon.h
+++ shapes/Polygon.h
@@ -4,25 +4,69 @@
 #include <QVector>
 #include "Shape.h"
 #include "../vector.h"
-
+//!A Polygon class
+/*!
+A class header file to construct a polygon
+*/
 namespace cs1c
 {
 class Polygon : public Shape
 {
 private:
-    QVector<QPoint> vPoints;
+    QVector<QPoint> vPoints; /*!< Points of the polygon */
 
 public:
+    //! A default constructor.
+    /*!
+    */
     Polygon();
+    //!An alternate constructor.
+    /*!
+    */
     Polygon(QPainter* pPainter);
+    //! An alternate constructor.
+    /*!
+    The points of the polygon is initialized with the constructor
+    */
     Polygon(QVector<QPoint> vPoint);
+    //! A destructor
+    /*!
+    Polygon object destroyed
+    */
     virtual ~Polygon();
+    //! A mutator function
+    /*!
+    The QPoints are set by passing the x,y coordinates in the function
+    */
     void setPoints(int, int);
+    //! An accessor function
+    /*!
+    Returns the coordinate points of the polygon
+    */
     QVector<QPoint>& getPoints();
+    //! A mutator function
+    /*!
+    */
     virtual void setDimensions(int dimensions[], int dimensionCount) override;
+    //! A draw function
+    /*!
+    Function draws the polygon
+    */
     virtual void draw(QPaintDevice *device) override;
+    //! A move function
+    /*!
+    Function to move the polygon
+    */
     virtual void move(int x, int y) override;
+    //! A perimeter function
+    /*!
+    Function returns the perimeter of the polygon
+    */
     virtual double perimeter() override;
+    //! An area function
+    /*!
+    Function returns the area of the polygon
+    */
     virtual double area() override;
 };
 }
